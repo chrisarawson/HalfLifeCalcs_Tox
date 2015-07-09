d0a996ee8400be6310cb9e9b92349de8fb9e3d68
Chris Rawson <c.rawson@curtin.edu.au>
2015-07-09 16:29:52 +0800

    Added mean lines to figures and outputs for means

--- halfLifeCalc.R
+++ halfLifeCalc.R
@@ -8,7 +8,7 @@
 #rVol = volume to be replaced (L) (default = 50% replacement)
 #rConc = Concentration of replacement volume (default = initial concentration)
 
-halfLifeCalc <- function (y0, tHalf,t, rInt, v0=1.5, rVol=0.5*v0, rConc=y0) {
+halfLifeCalc <- function (y0, tHalf,t, rInt, v0=1.5, rVol=0.5*v0, rConc=y0, lag=0) {
   #Generate x-data
   xPoints <- seq(0,t,length.out=(t*10)+1)
   
@@ -19,6 +19,7 @@ halfLifeCalc <- function (y0, tHalf,t, rInt, v0=1.5, rVol=0.5*v0, rConc=y0) {
   #plot as a line
   plot(decayData$xPoints,decayData$yPoints,type="l",col="black", ylim=c(0,y0), 
        xlab="Time (days)",ylab="Concentration (micro g/L)")
+  lines(x=c(0,max(xPoints)),y=c(average.y,average.y),col="red")
   
   #Generate sequence of replacement days
   rDays <- seq(0,t,by = rInt)
@@ -45,6 +46,20 @@ halfLifeCalc <- function (y0, tHalf,t, rInt, v0=1.5, rVol=0.5*v0, rConc=y0) {
   decayData$yRepPoints <- ryPoints
   decayData.out <<- decayData
   
+  #calculate average concentration during exposure
+  average.y <<- mean(decayData$yPoints)
+  average.ry <<- mean(decayData$yRepPoints)
+  
+  lagged.decayData <- subset(decayData.out,xPoints >= lag)
+  
+  average.ryLag <<-mean(lagged.decayData$yRepPoints)
+  
   plot(decayData$xPoints,ryPoints,type="l",xlab="Time (days)",ylab="Concentration (micro g/L)",
        ylim=c(0,max(ryPoints)))
+  lines(x=c(0,max(xPoints)),y=c(average.ry,average.ry),col="red")
+  lines(x=c(0,max(xPoints)),y=c(average.ryLag,average.ryLag),col="blue")
+  legend(-5,-8,xpd=TRUE,c("Mean with no lag","Mean with lag"),col=c("red","blue"),
+         lty=c(1,1),bty="n")
+  
+ 
 }
